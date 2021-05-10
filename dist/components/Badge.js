973ada59e60464f8478e833a0e8bb5e76a639777
Alexandru Pop <alexandru.pop@3pillarglobal.com>
2021-05-10 22:07:37 +0300

    initial setup for thecoffemug react library components

---
+++ dist/components/Badge.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Badge = () => {
+  return /*#__PURE__*/_react.default.createElement("h4", null, "This is my components BaDGE");
+};
+
+var _default = Badge;
+exports.default = _default;
---
+++ src/lib/components/Badge.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Badge = () => {
+    return(
+        <h4>This is my components BaDGE</h4>
+    );
+}
+export default Badge;
